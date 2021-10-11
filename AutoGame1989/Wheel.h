362f50a8e7f24f2701fc6209e0fe0ec73543709c
w.lehrjackson <w.lehrjackson@gmail.com>
2021-10-10 23:20:36 -0400

    Setting up wheel data

--- AutoGame1989/Wheel.h
+++ AutoGame1989/Wheel.h
@@ -1,5 +1,17 @@
 #pragma once
 
+
+// spring camera target
+struct WheelData
+{
+    int wheelID = 0;
+    bool isStearable = false;
+    DirectX::SimpleMath::Vector3 position = DirectX::SimpleMath::Vector3::Zero;
+    DirectX::SimpleMath::Vector3 forward = DirectX::SimpleMath::Vector3::UnitX;
+    DirectX::SimpleMath::Vector3 up = DirectX::SimpleMath::Vector3::UnitY;
+};
+
+
 class Wheel
 {
     public:
