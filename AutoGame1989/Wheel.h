d8f888a5ef867092e0739ceec1ad3f2e8a709a0d
w.lehrjackson <w.lehrjackson@gmail.com>
2021-10-11 22:14:49 -0400

    Building wheel data

--- AutoGame1989/Wheel.h
+++ AutoGame1989/Wheel.h
@@ -1,14 +1,26 @@
 #pragma once
 
 
-// spring camera target
+
 struct WheelData
 {
     int wheelID = 0;
     bool isStearable = false;
+    bool isGrounded;
     DirectX::SimpleMath::Vector3 position = DirectX::SimpleMath::Vector3::Zero;
     DirectX::SimpleMath::Vector3 forward = DirectX::SimpleMath::Vector3::UnitX;
     DirectX::SimpleMath::Vector3 up = DirectX::SimpleMath::Vector3::UnitY;
+
+    DirectX::SimpleMath::Vector3 axle;
+    DirectX::SimpleMath::Vector3 terrainNormal;
+    double radius;
+    double friction;
+    double sidewallHeight;
+    double width;
+    double tireDampening;
+    double mass;
+    double rpm;
+
 };
 
 
